409dd3517fe938d58e8b8dca78f5df231c37deff
Yusufhan ERBAŞ <63731504+Yusufhan-Erbas@users.noreply.github.com>
2023-05-07 13:27:35 +0300

    Character Movement script written.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Rigidbody2D rb;
+	Animator anim;
+    public float  speed=5f;
+	bool isRun = false;
+	bool isRight = true;
+	private void Start()
+	{
+		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void FixedUpdate()
+	{
+		Move();
+	}
+	#region Character Movement
+	void Move()
+	{
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(Vector2.right*speed*Time.deltaTime);
+			isRun = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(Vector2.left *speed* Time.deltaTime);
+			isRun = true;
+		}
+		anim.SetBool("isRunning",isRun);
+		isRun = false;
+	}
+	#endregion
+
+	#region Character Turn
+	void Turn()
+	{
+		
+	}
+	#endregion
 }
